dac3a72637b3f59da43026045fc7f94df7c432cb
Alexey <mailofauroraborealis@gmail.com>
2020-11-09 16:03:10 +0300

    Test View

---
+++ view/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#/">Главная</b-nav-item>
+            <b-nav-item href="#/upload">Загрузить</b-nav-item>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                <em>Пользователь</em>
+              </template>
+              <b-dropdown-item href="#">Профиль</b-dropdown-item>
+              <b-dropdown-item href="#">Выход</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+
+      <router-view/>
+  </div>
+</template>
+
+<style lang="stylus">
+.custom-file-input:lang(ru)
+  ~ .custom-file-label
+    &::after
+      content 'Просмотр'
+
+</style>
---
+++ view/src/views/Upload.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-container>
+
+    <b-tabs v-model="tabIndex" content-class="mt-3" fill>
+
+      <b-tab title="Загрузить">
+
+        <b-form-file
+          v-model="file"
+          :state="Boolean(file)"
+          placeholder="Выберите или перетащите файл..."
+          drop-placeholder="Перетащите сюда..."
+          size="lg"
+        ></b-form-file>
+        <div class="mt-3">Выбранный файл: {{ file ? file.name : '' }}</div>
+        <b-button @click="upload" :disabled="!file" class="mr-2">Загрузить</b-button>
+
+      </b-tab>
+      <b-tab title="Просмотр" lazy>
+        <template #title>
+          <b-spinner type="border" small v-if="loading"></b-spinner> Просмотр
+        </template>
+
+        <b-table striped hover :items="items"></b-table>
+
+      </b-tab>
+
+    </b-tabs>
+
+  </b-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      file: null,
+      loading: false,
+      tabIndex: 0,
+      items: [
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' }
+      ]
+    }
+  },
+  methods: {
+    upload () {
+      console.log('Загрузить файл')
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+        this.tabIndex = 1
+      }, 4000)
+    }
+  }
+}
+</script>
